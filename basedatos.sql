5491b43bee4d11367316a2c4dc97e99c3a84cc21
MalejaTM <87444493+MalejaTM@users.noreply.github.com>
2021-07-14 16:43:16 -0500

    Versión 1 de BD

---
+++ basedatos.sql
@@ -0,0 +1,20 @@
+create database biblioteca;
+use biblioteca;
+create table información_usuario(
+id_cliente varchar(10),
+nombre varchar(25),
+dirección varchar(25),
+telefono varchar (10),
+primary key(id_Usuario)
+);
+
+create table categoria(
+id_categoria varchar(10),
+nombre varchar(25),
+primary key(id_categoria)
+);
+create table libro(...)
+
+insert into  values ('555', 'ana', 'belen'), ('444','Leo', 'Roldán');
+
+
